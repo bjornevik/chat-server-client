4ad4279793fadeda5e2907d52339623cb50bf5f4
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-09-07 14:14:33 +0200

    feat: feature "complete" client

--- client/main.go
+++ client/main.go
@@ -1,28 +1,91 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
+	"time"
 )
 
-// TODO: Better handling of env variables
 const (
-	SERVER_HOST = "localhost"
-	SERVER_PORT = "8081"
-	SERVER_TYPE = "tcp"
+	SERVER_HOST    = "localhost"
+	SERVER_PORT    = "8081"
+	SERVER_TYPE    = "tcp"
+	RETRY_DURATION = 1 * time.Minute // Try to reconnect for 1 minute before exiting
+	RETRY_INTERVAL = 5 * time.Second // Try to reconnect every 5 seconds
 )
 
 func main() {
-	conn, err := net.Dial(SERVER_TYPE, fmt.Sprintf("%v:%v", SERVER_HOST, SERVER_PORT))
+	var conn net.Conn
+	var err error
+
+	conn, err = connectWithRetry()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 
-	_, err = conn.Write(([]byte("Hello, world")))
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println("Connected to the server")
+
+	// Listen to broadcasted messages
+	go listen(conn)
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// handler(conn, reader)
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		_, err := conn.Write([]byte(input + "\n"))
+		if err != nil {
+			fmt.Println("Connection lost. Attempting to reconnect...")
+			conn, err = connectWithRetry()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println("Connected to the server!")
+			continue
+		}
+	}
+}
+
+func listen(conn net.Conn) {
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Connection lost. Attempting to reconnect...")
+			conn, err = connectWithRetry()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println("Connected to the server!")
+			continue
+		}
+
+		fmt.Print("\r" + message + "> ")
+	}
+}
+
+func connectWithRetry() (net.Conn, error) {
+	startTime := time.Now()
+
+	for {
+		conn, err := net.Dial(SERVER_TYPE, fmt.Sprintf("%v:%v", SERVER_HOST, SERVER_PORT))
+		if err == nil {
+			return conn, nil
+		}
+
+		if time.Since(startTime) > RETRY_DURATION {
+			return nil, fmt.Errorf("Failed to reconnect: timeout after 1 minute")
+		}
+
+		fmt.Println("Failed to connect, retrying in 5 seconds")
+		time.Sleep(RETRY_INTERVAL)
 	}
 }
